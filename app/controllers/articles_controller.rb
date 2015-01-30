50dc77f8dde6423ba6d875ee47ffc5abede0e513
Miroslav Macura <mmacura@QBS-J-L-0264.local>
2015-01-30 10:46:08 +0200

    form-based workflow feature complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,43 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     def index
         @articles = Article.all
     end
     def show
         @article = Article.find(params[:id])
+    end
+    def new
+        @article = Article.new
+    end
+    def create
+        #@article = Article.new(article_params)
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.body = params[:article][:body]
+        @article.save
+        flash.notice = "New article '#{@article.title}' created!"
+        redirect_to article_path(@article)
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash.notice = "Article '#{@article.title}' deleted!"
+        redirect_to articles_path
+    end
+    def edit
+       @article = Article.find(params[:id]) 
+    end
+    def update
+      @article = Article.find(params[:id])
+      @article.title = params[:article][:title]
+      @article.body = params[:article][:body]
+      @article.save        
+      #@article.update(@article.title, @article.body)
+      #@article.update(article_params) - http://stackoverflow.com/questions/16468487/rails-4-strong-params-error-no-implicit-conversion-of-symbol-into-string
+      #Article.update( params[:id], params[:article][:title], params[:article][:body])    
+      # for rails <4  - http://stackoverflow.com/questions/27199089/private-method-update-called-for-customer
+       # @article.update_attributes( :body => params[:article][:body])
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article)
     end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
 module ArticlesHelper
+    
+    def article_params
+        params.require(:article).permit(:title, :body)
+        #params.require(:article).permit(:title)
+        #params.require(:article).permit(:body)
+    end
 end
